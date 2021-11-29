d497e10f0e0bd5a52297b792cd0c2d42f33af57e
Xueting Hu <huxu@uwplatt.edu>
2021-11-28 23:14:36 -0600

    Updated a few bugs

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -53,28 +53,16 @@ const thoughtController = {
     //update thought by id 
     updateThought({ params, body }, res) {
         Thought.findOneAndUpdate(
-            { _id: params.id },
-            body,
-            { new: true, runValidators: true}
-        )
-        .then((dbThoughtData) => {
-            console.log(body.userId)
-            console.log(dbThoughtData._id)
-            return User.findOneAndUpdate(
-                { _id: body.userId },
-                { $push: { thoughts: dbThoughtData._id }},
-                { new: true}
-            )
-        })
-        .then(dbUserData => {
-            if(!dbUserData) {
-                res.status(404).json({ message: 'no user found with this id'});
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => res.json(err));
-        },
+            { _id: params.id }, body, { new: true })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with that id.' })
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
 
     //delete thought
     deleteThought({ params }, res) {
